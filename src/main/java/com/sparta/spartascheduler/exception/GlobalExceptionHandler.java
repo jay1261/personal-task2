8491c5dbca03d5c2521de1b8d4d414e3dc97dba5
jay1261 <dongjae73@naver.com>
2024-05-31 09:07:33 +0900

    개인과제2[5단계] - JWT (uitl 생성)

--- src/main/java/com/sparta/spartascheduler/exception/GlobalExceptionHandler.java
+++ src/main/java/com/sparta/spartascheduler/exception/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.sparta.spartascheduler.exception;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.coyote.BadRequestException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.Errors;
 import org.springframework.validation.FieldError;
@@ -17,6 +18,19 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Map<String, String>> exampleResponseValidation(MethodArgumentNotValidException e) {
+        Map<String, String> error = new HashMap<>();
+        e.getAllErrors().forEach(
+                c -> {
+                    error.put(((FieldError) c).getField(), c.getDefaultMessage());
+                    log.error(c.getDefaultMessage());
+                }
+        );
+
+        return ResponseEntity.badRequest().body(error);
+    }
+
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<Map<String, String>> handleBadRequestException(MethodArgumentNotValidException e) {
         Map<String, String> error = new HashMap<>();
         e.getAllErrors().forEach(
                 c -> {
